d083303f760976b05fcee50a923b5b0df7de96e7
Suman Bhowmik <sumanbhowmik700@gmail.com>
2022-10-24 18:55:43 +0530

    Java program to print day name of weekend using switch statement

---
+++ Weekend.java
@@ -0,0 +1,34 @@
+/* Enter code of Day and Print Nane of Day */
+
+import java.util.Scanner;
+
+class Weekend
+{
+    public static void main(String[] args){
+
+        int n;
+        System.out.print("Enter code of Day ");
+        Scanner r =new Scanner(System.in);
+        n = r.nextInt();
+
+        switch(n)
+        {
+            case 0: System.out.println(" Sunday"); 
+            break;
+            case 1: System.out.println(" Monday");
+            break;
+            case 2: System.out.println(" Tuesday");
+            break;
+            case 3: System.out.println(" Wednesday");
+            break;
+            case 4: System.out.println(" Thursday");
+            break;
+            case 5: System.out.println(" Friday");
+            break;
+            case 6: System.out.println(" Saturday");
+            default : System.out.println("Invalid code");
+            break;
+
+        }
+    }
+}
