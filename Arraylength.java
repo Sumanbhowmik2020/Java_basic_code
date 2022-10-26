9287a1b0ba8fb03d52d71ec0ca62c3811d4d76e4
Suman Bhowmik <sumanbhowmik700@gmail.com>
2022-10-26 20:00:13 +0530

    Java program to find length of array

---
+++ Arraylength.java
@@ -0,0 +1,23 @@
+/* Find length of Arrays */
+
+import java.util.Scanner;
+
+class Arraylength
+{
+    public static void main(String[] args) {
+
+        int a[]=new int[5];
+        Scanner r=new Scanner(System.in);
+        System.out.print("Enter elements in array");
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=r.nextInt();
+        }
+        System.out.print("Array elements");
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+        System.out.print("Length of array is "+a.length);
+    }
+}
