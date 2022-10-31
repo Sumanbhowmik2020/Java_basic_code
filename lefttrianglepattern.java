597a0152823f266c4fb6905494df10e7f1b960ad
Suman Bhowmik <sumanbhowmik700@gmail.com>
2022-10-31 20:20:35 +0530

    Left Triangle Star Pattern in Java

---
+++ lefttrianglepattern.java
@@ -0,0 +1,21 @@
+/* Left Triangle Star Pattern */
+
+class Lefttrianglepattern
+{
+    public static void main(String[] args) {
+
+        int i,j,k;
+        for(i=1;i<=5;i++) //raws
+        {
+            for(j=i;j<5;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
