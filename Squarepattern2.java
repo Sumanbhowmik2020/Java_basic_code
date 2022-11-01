f1d7893861ace4febdaf1127c6a9ba0133cf97cc
Suman Bhowmik <sumanbhowmik700@gmail.com>
2022-11-01 20:53:50 +0530

    Square Star Pattern (type 2) inside blank in Java

---
+++ Squarepattern2.java
@@ -0,0 +1,35 @@
+/* Square Star Pattern (type 2) */
+
+/* 
+
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+
+*/
+
+class Squarepattern2
+{
+    public static void main(String[] args) {
+
+        int i,j;
+        for (i=1;i<=5;i++)
+        {
+            for (j=1;j<=5;j++)
+            {
+                if(i==1 || i==5 || j==1 || j==5)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.print("\n");
+        }
+    }
+}
