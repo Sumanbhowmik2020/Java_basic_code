21ff2320a5074624d93e0b17350ce793035ef808
Sumanbhowmik2020 <sumanbhowmik700@gmail.com>
2023-07-02 19:39:55 +0530

    //Decreasing Triangle star pattern using JAVA

---
+++ pattern/star/DecreasingTriangle.java
@@ -0,0 +1,12 @@
+//Decreasing Triangle star pattern using JAVA
+class DecreasingTriangle{  
+    public static void main(String args[]){  
+        int n=5;
+        for (int i=1; i<=n; i++){
+            for (int j=i; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }  
+}
