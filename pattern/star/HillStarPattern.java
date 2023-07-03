e7b8e2c699f2924f7dc1b44492ddca5d66f9b60d
Sumanbhowmik2020 <sumanbhowmik700@gmail.com>
2023-07-03 17:25:58 +0530

    //Hill star pattern using JAVA

---
+++ pattern/star/HillStarPattern.java
@@ -0,0 +1,26 @@
+//Hill star pattern using JAVA
+
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+
+
+class HillStarPattern{  
+    public static void main(String args[]){  
+        int n=5;
+        for (int i=1; i<=n; i++){
+            for (int j=i; j<=n; j++){
+                System.out.print("  "); // two space
+            }
+            for (int j=1; j<i; j++){
+                System.out.print("* "); // one space after star
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("* "); // one space after star
+            }
+            System.out.println();
+        }
+    }  
+}
