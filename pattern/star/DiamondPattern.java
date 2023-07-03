ae3b7c116c0d361e10e35daaad20bb6fa37bd50a
Sumanbhowmik2020 <sumanbhowmik700@gmail.com>
2023-07-03 19:26:45 +0530

    //Diamond star pattern using JAVA

---
+++ pattern/star/DiamondPattern.java
@@ -0,0 +1,42 @@
+//Diamond star pattern using JAVA
+
+//         *
+//       * * *
+//     * * * * *
+//   * * * * * * *
+// * * * * * * * * *
+//   * * * * * * *
+//     * * * * *
+//       * * *
+//         *
+
+
+class DiamondPattern{  
+    public static void main(String args[]){  
+        int n=5;
+        for (int i=1; i<n; i++){
+            for (int j=i; j<=n; j++){
+                System.out.print("  "); // two space
+            }
+            for (int j=1; j<i; j++){
+                System.out.print("* "); // one space after star
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("* "); // one space after star
+            }
+            System.out.println();
+        }
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("  "); // two space
+            }
+            for (int j=i; j<n; j++){
+                System.out.print("* "); // one space after star
+            }
+            for (int j=i; j<=n; j++){
+                System.out.print("* "); // one space after star
+            }
+            System.out.println();
+        }
+    }  
+}
