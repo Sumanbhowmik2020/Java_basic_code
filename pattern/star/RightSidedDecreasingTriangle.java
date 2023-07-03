09af45e041548f377788c7938faf8bd8d7564062
Sumanbhowmik2020 <sumanbhowmik700@gmail.com>
2023-07-03 13:58:04 +0530

    //RightSidedDecreasingTriangleStarPatternUsingJAVA

---
+++ pattern/star/RightSidedDecreasingTriangle.java
@@ -0,0 +1,22 @@
+//Right Sided Decreasing Triangle star pattern using JAVA
+
+// * * * * *
+//   * * * *
+//     * * *
+//       * *
+//         *
+
+class RightSidedDecreasingTriangle{  
+    public static void main(String args[]){  
+        int n=5;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("  "); // two space
+            }
+            for (int j=i; j<=n; j++){
+                System.out.print("* "); // one space after star
+            }
+            System.out.println();
+        }
+    }  
+}
